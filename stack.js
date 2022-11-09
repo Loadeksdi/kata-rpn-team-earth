40ec1a700f3bc2daf5ee2ee0b802497c30b7ada1
Loadeksdi <nicolas.load@gmail.com>
2022-11-09 11:13:55 +0100

    2-Adding item to stack

--- stack.js
+++ stack.js
@@ -2,6 +2,12 @@ class Stack{
     constructor(){
         this.stack = [];
     }
+    push(value){
+        this.stack.push(value);
+    }
+    top(){
+        return this.stack[this.stack.length-1];
+    }
 }
 
 module.exports = Stack;
--- tests/stack.test.js
+++ tests/stack.test.js
@@ -5,4 +5,9 @@ describe('Stack', () => {
         const stack = new Stack();
         expect(stack).toBeInstanceOf(Stack);
     });
+    it('add 20 to stack', () => {
+        const stack = new Stack();
+        stack.push(20);
+        expect(stack.top()).toBe(20);
+    });
 });
